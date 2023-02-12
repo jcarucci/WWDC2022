a07a2d486340954ede1a4ecf7cc92eff622deab3
Jason Carucci <jcarucci@gmail.com>
2023-02-12 11:48:59 -0500

    Add Locations detail chart

---
+++ HelloSwiftCharts/LocationsDetailsChart.swift
@@ -0,0 +1,20 @@
+//
+//  LocationsDetailsChart.swift
+//  HelloSwiftCharts
+//
+//  Created by Jason on 2/12/23.
+//
+
+import SwiftUI
+
+struct LocationsDetailsChart: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LocationsDetailsChart_Previews: PreviewProvider {
+    static var previews: some View {
+        LocationsDetailsChart()
+    }
+}
