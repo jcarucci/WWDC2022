0037b5eb6ed269a20435903013223e574c8eae29
Jason Carucci <jcarucci@gmail.com>
2023-02-12 11:45:30 -0500

    Add picker to toggle between cities

--- HelloSwiftCharts/HelloSwiftCharts/LocationsToggleChart.swift
+++ HelloSwiftCharts/HelloSwiftCharts/LocationsToggleChart.swift
@@ -5,11 +5,49 @@
 //  Created by Jason on 2/12/23.
 //
 
+import Charts
 import SwiftUI
 
+enum City {
+    case cupertino
+    case sanFrancisco
+}
+
+let sfData: [SalesSummary] = [
+    /// Monday
+    .init(weekday: makeDate(2022, 5, 2), sales: 54),
+    /// Tuesday
+    .init(weekday: makeDate(2022, 5, 3), sales: 42),
+    /// Wednesday
+    .init(weekday: makeDate(2022, 5, 4), sales: 88),
+    /// Thursday
+    .init(weekday: makeDate(2022, 5, 5), sales: 49),
+    /// Friday
+    .init(weekday: makeDate(2022, 5, 6), sales: 42),
+    /// Saturday
+    .init(weekday: makeDate(2022, 5, 7), sales: 125),
+    /// Sunday
+    .init(weekday: makeDate(2022, 5, 8), sales: 67)
+]
+
 struct LocationsToggleChart: View {
+    @State var city: City = .cupertino
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Picker("City", selection: $city) {
+                Text("Cupertino").tag(City.cupertino)
+                Text("San Francisco").tag(City.sanFrancisco)
+            }
+            .pickerStyle(.segmented)
+            Chart(cupertinoData) { element in
+                BarMark(x: .value("Day", element.weekday),
+                        y: .value("Sales", element.sales)
+                )
+            }
+        }
+        .padding()
     }
 }
 
