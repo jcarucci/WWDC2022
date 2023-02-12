aa43ce07f8e07ae53b5d8fec981a8f26db6d5324
Jason Carucci <jcarucci@gmail.com>
2023-02-12 11:40:33 -0500

    Add Locations toggle chart

---
+++ HelloSwiftCharts/HelloSwiftCharts/LocationsToggleChart.swift
@@ -0,0 +1,20 @@
+//
+//  LocationsToggleChart.swift
+//  HelloSwiftCharts
+//
+//  Created by Jason on 2/12/23.
+//
+
+import SwiftUI
+
+struct LocationsToggleChart: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LocationsToggleChart_Previews: PreviewProvider {
+    static var previews: some View {
+        LocationsToggleChart()
+    }
+}
